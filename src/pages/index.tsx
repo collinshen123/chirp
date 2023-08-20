da5d1b4fa6e38de5e51188cc2b9c292756c7b7a8
collinshen123 <collinshen123@gmail.com>
2023-08-20 11:01:09 -0700

    post updating

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -19,7 +19,14 @@ const CreatePostWizard = () => {
 
   const [input, setInput] = useState("");
 
-  const { mutate } = api.posts.create.useMutation();
+  const ctx = api.useContext();
+
+  const { mutate, isLoading: isPosting } = api.posts.create.useMutation({
+    onSuccess: () => {
+      setInput("");
+      void ctx.posts.getAll.invalidate();
+    },
+  });
 
   console.log(user);
 
@@ -40,6 +47,7 @@ const CreatePostWizard = () => {
       type="text"
       value={input}
       onChange={(e) => setInput(e.target.value)}
+      disabled={isPosting}
     /> 
     <button onClick={() => mutate({content: input })}>Post</button>
   </div>
