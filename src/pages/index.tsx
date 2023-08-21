7dc1f2bc844e1ab44a97f17f484a024d6e761dbb
collinshen123 <collinshen123@gmail.com>
2023-08-21 10:15:30 -0700

    routing to profile and post page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,6 +12,7 @@ import { LoadingPage, LoadingSpinner } from "~/components/loading";
 import { useState } from "react";
 import toast from "react-hot-toast";
 import { LOADIPHLPAPI } from "dns";
+import Link from "next/link";
 dayjs.extend(relativeTime);
 
 
@@ -99,8 +100,8 @@ const PostView = (props: PostWithUser) => {
       />
       <div className="flex flex-col">
         <div className="flex gap-1 text-slate-600">
-          <span>{`@${author.username}`}</span>
-          <span className=" text-slate-400">{`· ${dayjs(post.createdAt).fromNow()}`}</span>
+          <Link href={'/@${author.username}'}><span>{`@${author.username}`}</span></Link>
+          <Link href={'/post/${post.id}'}> <span className=" text-slate-400">{`· ${dayjs(post.createdAt).fromNow()}`}</span></Link>
         </div>
         <span className="text-black"> {post.content} </span>
       </div>
